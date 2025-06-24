11f059e813af48750d4966cc0096217be06c53be
AbdelmageedBaher <abdelmageedbaher12@gmail.com>
2025-06-24 19:28:57 +0300

    save

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
+<QueryClientProvider client={queryClient}>
     <App />
+</QueryClientProvider>
 )
