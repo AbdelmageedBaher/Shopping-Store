40fe757e64d416d8b4b47a82f0e406b0b38947ce
samehwad122 <samehwad2@gmail.com>
2025-06-24 19:13:53 +0300

    shop page + folder context + apiContext + folder hooks + userShop

---
+++ src/components/Shop/PurchaseOrder/PurchaseOrder.jsx
@@ -0,0 +1,15 @@
+// templates/component/PurchaseOrder.js
+
+import React from 'react';
+import PurchaseOrder from './PurchaseOrder.css';
+
+export default function PurchaseOrder() {
+    return(
+
+        <div className='PurchaseOrder'>
+          <h1>PurchaseOrder component</h1>
+        </div>
+    )
+};
+
+ 
