0bc1bc31777d9e9debdd21ab06dde2a155817bac
samehwad122 <samehwad2@gmail.com>
2025-06-24 22:15:08 +0300

    slick

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import './index.css'
 import App from './App.jsx'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
