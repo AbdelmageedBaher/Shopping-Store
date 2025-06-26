1a0b8ffe87d95900fce875d6558e8747275f79c3
AbdelmageedBaher <abdelmageedbaher12@gmail.com>
2025-06-26 12:26:47 +0300

    update main.jsx and instalation libraries

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
+import { ApiContextProvider } from './context/apiContext.jsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-    <App />
+
+   <QueryClientProvider client={queryClient}>
+    <ApiContextProvider>
+       <App />
+       </ApiContextProvider>
+   </QueryClientProvider>
 )
