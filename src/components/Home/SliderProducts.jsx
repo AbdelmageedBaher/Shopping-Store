4b328b0e3f2115761b6f20bffd8d92cd5c0abb1d
Ahmed-koryem <ahmedkoryem44@gmail.com>
2025-07-02 23:34:39 +0200

    edit navbar and footer

--- src/components/Home/SliderProducts.jsx
+++ src/components/Home/SliderProducts.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query"
 import Slider from "react-slick";
 
 export default function SliderProducts() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './index.css'
 import App from './App.jsx'
 import { ApiContextProvider } from './context/apiContext.jsx';
