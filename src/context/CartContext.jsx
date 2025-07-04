8b7edfc3f4b48abeb4e5d8d45b75e0fd46e3a628
Khaled Melgharib <khaledmelgharib@gmail.com>
2025-07-04 15:38:46 +0300

    Refactor CartContext to enhance cart item management and improve local storage handling

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -71,79 +71,6 @@ export const CartContextProvider = ({ children }) => {
     return cartItems.reduce((total, item) => total + item.quantity, 0);
   };
 
-<<<<<<< HEAD
-export const CartContextProvaider = ({children})=>{
-    
-    const [cart , setCart] = useState([])
-
-useEffect(()=>{
-    if(localStorage.getItem("cartData")){
-        setCart(JSON.parse(localStorage.getItem("cartData")))
-        
-    }else{
-        setCart([])
-    }
-} , [])
-    
-    useEffect(()=>{
-    localStorage.setItem("cartData" , JSON.stringify(cart))
-    } , [cart])
-
-
-
-function addToCart(product) {
-    const findProduct=cart.find((el)=> el.title === product.title) 
-
-    if (!findProduct) {
-            setCart([...cart , {...product , amount : 1}])
-    }else{
-              findProduct.amount +=1              
-    }
-
-}
-
-function deleteFromCart(product) {
-    const newArr = cart.filter((el)=>  el.title !== product.title)
-    setCart(newArr)
-}
-
-function changeAmount(state , product) {
- if(state === "plus"){
-    ++product.amount
-    setCart([...cart])
-}else{
-    if(product.amount > 0){
-        --product.amount
-        
-        setCart([...cart])
-    }else{
-        deleteFromCart(product)
-    }
- }
-}
-const totallCart = cart.reduce((a , b)=> {
-    return a + (b.price * b.amount)
-} , 0 )
-
-const cartLength = cart.reduce((a , b)=>{return a+b.amount} , 0)
-
-
-
-
-
-
-
-
-return <cartContext.Provider value={{addToCart , cart , deleteFromCart , changeAmount , totallCart , cartLength}}>
-
-{children}
-
-
-</cartContext.Provider>
-
-
-}
-=======
   return (
     <CartContext.Provider
       value={{
@@ -151,8 +78,8 @@ return <cartContext.Provider value={{addToCart , cart , deleteFromCart , changeA
         addToCart,
         deleteFromCart, // Navbar يتوقع 'deleteFromCart'
         changeAmount,   // Navbar يتوقع 'changeAmount'
-        totallCart,     // Navbar يتوقع 'totallCart'
-        cartLength,     // Navbar يتوقع 'cartLength'
+        totallCart: getSubtotal(),     // Navbar يتوقع 'totallCart'
+        cartLength: getTotalItems(),   // Navbar يتوقع 'cartLength'
       }}
     >
       {children}
